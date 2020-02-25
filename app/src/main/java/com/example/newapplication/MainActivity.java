3889350ada5ee447d98cbed607a756c10bad0e9d
Lovro Frece <lovro.frece@gmail.com>
2020-02-25 16:04:22 +0100

    nekaj malega na login pagu

--- app/src/main/java/com/example/newapplication/MainActivity.java
+++ app/src/main/java/com/example/newapplication/MainActivity.java
@@ -6,15 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView ustvariNovRacun;
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Prijava");
 
+        ustvariNovRacun =findViewById(R.id.textViewUstvariNovRacun);
         button = findViewById(R.id.buttonLogin);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -25,7 +28,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        setTitle("Prijava");
+        ustvariNovRacun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), CreateUserActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
