ef937dcc49509cf6609dc87d3cd2bfea9a1d77c3
Lovro Frece <lovro.frece@gmail.com>
2020-02-25 11:25:02 +0100

    uredil logo, fragmenti zdej lepo delajo

--- app/src/main/java/com/example/newapplication/FirstPageActivity.java
+++ app/src/main/java/com/example/newapplication/FirstPageActivity.java
@@ -16,7 +16,35 @@ public class FirstPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();// container s fragmenti se odpre ob zagonu aplikacije
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_redovalnica:
+                    selectedFragment = new RedovalnicaFragment();
+                    break;
+                case R.id.nav_refresh:
+                    selectedFragment = new ObnoviFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+            return true; //izberemo item, lahko tudi false(fragment se bo prikazal, item pa ne bo izbran)
+        }
+    };
 
 
 }
